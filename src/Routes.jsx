be6a4e4b4ae128f1a6671177fbf6a21897b34fcc
Jason Dagana <j.dagana@teflonhub.com>
2025-09-27 06:34:55 +0100

    changed the base route for my portfolio

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -16,9 +16,9 @@ const Routes = () => {
       <ScrollToTop />
       <RouterRoutes>
         {/* Define your route here */}
-        <Route path="/" element={<AboutProfessional />} />
+        <Route path="/" element={<HomeLanding />} />
         <Route path="/portfolio-projects" element={<PortfolioProjects />} />
-        <Route path="/home-landing" element={<HomeLanding />} />
+        {/* <Route path="/home-landing" element={<HomeLanding />} /> */}
         <Route path="/technical-blog" element={<TechnicalBlog />} />
         <Route path="/contact-connect" element={<ContactConnect />} />
         <Route path="/about-professional" element={<AboutProfessional />} />
