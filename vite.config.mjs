f79fce191dfb763f9cb6d48e24137651ecccd6d2
Jason Dagana <j.dagana@teflonhub.com>
2025-09-27 07:57:21 +0100

    refresh cache for seo trigger

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,10 +7,11 @@ import tagger from "@dhiwise/component-tagger";
 export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
-  // build: {
-  //   outDir: "build",
-  //   chunkSizeWarningLimit: 2000,
-  // },
+  build: {
+  outDir: "dist",
+  assetsDir: "assets",
+  rollupOptions: {},
+},
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
     port: "3000",
