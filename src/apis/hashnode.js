95d0ffca0c315883ad7239e441de82c11eb2b995
Jason Dagana <j.dagana@teflonhub.com>
2026-05-18 12:58:27 +0100

    added body parser

--- src/apis/hashnode.js
+++ src/apis/hashnode.js
@@ -1,3 +1,9 @@
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
@@ -7,14 +13,13 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
   const response = await fetch("https://gql.hashnode.com", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    //   ...(process.env.HASHNODE_API_KEY && {
-    //     Authorization: process.env.HASHNODE_API_KEY
-    //   })
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(req.body),
   });
 
